4bc4e8008c8f8aa61cee57e2032457b6d57f7ed1
gyurybc <gyurybc@kiva.com>
2022-01-14 20:56:48 +0800

    ybc second commit

--- src/main/java/com/ybc/git/gittest.java
+++ src/main/java/com/ybc/git/gittest.java
@@ -3,6 +3,7 @@ package com.ybc.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 
 }
